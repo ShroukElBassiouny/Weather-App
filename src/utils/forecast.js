055b2e5ba718b952a40971ebd076a81204b984a9
ShroukElBassiouny <shroukelbassiony@gmail.com>
2022-10-24 18:35:14 +0200

    fix : handling error

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,13 +1,15 @@
 const request = require('request')
 const forecast = (latitude , longitude , callback)=>{
     const url = `http://api.weatherstack.com/current?access_key=688bc178f96b2aa3b0d0fa2c0b04894d&query=${latitude},${longitude}&units=m`
-    request({url, json : true}, (error , {body})=>{
-        if (error){
-            callback({error: "Unable to connect weather service!"}, undefined)
-        }else if(body.error){
-            callback({error :"Unable to find location!"}, undefined)
-        }else{
-            callback(undefined,`${body.current.weather_descriptions[0]} : It's currently ${body.current.temperature} degrees out. And it's feel like ${body.current.feelslike} degrees out.The humidity is ${body.current.humidity}%.`)
+    request({url, json : true}, (error , {body}={})=>{
+        try{
+            if (error){
+                callback("Unable to connect weather service!", undefined)
+            }else{
+                callback(undefined,`${body.current.weather_descriptions[0]} : It's currently ${body.current.temperature} degrees out. And it's feel like ${body.current.feelslike} degrees out.The humidity is ${body.current.humidity}%.`)
+            }
+        }catch(e){
+           callback ('Oops , something went wrong try in another time!')
         }
     })
 }
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,18 +1,23 @@
 const request = require('request')
 const geocode = (address , callback)=>{
     const url = `http://api.positionstack.com/v1/forward?access_key=9546069bb53390d310312c1623b33eb1&query=${encodeURIComponent(address)}&limit=1`
-    request({url, json : true}, (error , {body})=>{
-        if (error){
-            callback("Unable to connect location service!" , undefined)
-        }else if(body.data.length === 0 ){
-            callback("Unable to find location! , Try anther search" , undefined)
-        }else {
-            callback(undefined,{
-                latitude: body.data[0].latitude,
-                longitude: body.data[0].longitude,
-                location: body.data[0].label
-        })
-    }
+    request({url, json : true}, (error , {body}={})=>{
+        try{
+            if (error){
+                callback ("Unable to connect location service!", undefined)
+            }else if(body.data.length === 0 ){
+                callback("Unable to find location! , Try anther search" , undefined)
+            }else {
+                callback(undefined,{
+                    latitude: body.data[0].latitude,
+                    longitude: body.data[0].longitude,
+                    location: body.data[0].label
+            })
+        }
+        }catch(e){
+            callback ('Oops , something went wrong try in another time!')
+        }
+        
     })
 }
 module.exports = geocode
